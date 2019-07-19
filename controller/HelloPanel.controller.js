2a6e9d08d94d7f8f1aa067b1528d872ef65d5242
tomas-fuerl <tomasfuerl@googlemail.com>
2019-07-19 08:14:46 +0000

    Berechnung

--- controller/HelloPanel.controller.js
+++ controller/HelloPanel.controller.js
@@ -12,8 +12,8 @@ sap.ui.define([
 			var zahl1 = this.getView().getModel().getProperty("/recipient/zahl1");
             var zahl2 = this.getView().getModel().getProperty("/recipient/zahl2");
             var ergebnis = zahl1 + zahl2
-			var sMsg1 = oBundle.getText("helloMsg1");
-            var sMsg2 = oBundle.getText("helloMsg2");
+			var sMsg1 = parseFloat(oBundle.getText("helloMsg1"));
+            var sMsg2 = parseFloat(oBundle.getText("helloMsg2"));
 			// show message
 			MessageToast.show(zahl1 + sMsg1 + zahl2 + sMsg2 + ergebnis);
 		},
